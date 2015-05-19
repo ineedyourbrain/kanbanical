207079ce316686ec0e4c202b511249f204606db8
Leigh Frampton <leigh765@me.com>
2015-05-19 13:09:09 -0400

    4

--- timeblocker/app/components/trello-ui/trelloUICntrl.js
+++ timeblocker/app/components/trello-ui/trelloUICntrl.js
@@ -1,26 +1,27 @@
-
+console.log("bob");
 
 var app = angular.module('timeblocker');
 
 app.controller('trelloUI', function ($scope) {
 	$scope.cardselect = false;
 	$scope.selectedItems =[];
-	$scope.setCards =function (item) {
+	$scope.setCards = function (item) {
+		console.log(item);
 		$scope.cardselect = true;
 		$scope.selectedItems.push(item);
-		console.log($scope.selectedItems);
-		$scope.event = function() { 
-		var location_add= board.name +"."+card.name;
-		var title_add= board.name +": "+card.name;
-		var eventData = { 
-			title: title_add, 
-			details: 'details_add', 
-			location: location_add, 
-			allDay: false,
-			calendar: "coding"
-			};
+
+		// $scope.event = function() { 
+		// var location_add= board.name +"."+card.name;
+		// var title_add= board.name +": "+card.name;
+		// var eventData = { 
+		// 	title: title_add, 
+		// 	details: 'details_add', 
+		// 	location: location_add, 
+		// 	allDay: false,
+		// 	calendar: "coding"
+		// 	};
 			
-		google.calendar.composeEvent(eventData); 
+		// google.calendar.composeEvent(eventData); 
 
 
 	}
